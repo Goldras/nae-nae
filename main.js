33168f21e04c3f85cb9bba5936972306936794cf
Goldra <ultragavin229@gmail.com>
2019-09-18 21:20:39 -0700

    Update main.js

--- main.js
+++ main.js
@@ -1,8 +1,27 @@
-var x = 0
-function coolsauce(){
-x += 1
-if(x > Number.MAX_SAFE_INTEGER){
-    x = infinity
-  }
+var second = 0
+var minutes = 0
+var hours = 0
+var days = 0
+var years = 0
+function globalTimer(){
+ if(second = 60){
+    second = 0
+    minutes = 1
+    if(minutes = 60){
+        minutes = 0
+        hours = 1
+        if(hours = 24){
+            hours = 0
+            days = 1
+            if(days = 365){
+               days = 0
+               years = 1
+        }
+    }
 }
-setInterval(coolsauce, 10);
+}
+     console.log([second, minutes, hours, days, years]);
+     second ++
+ }
+
+setInterval(globalTimer, 1000);
