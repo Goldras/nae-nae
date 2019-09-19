a6b3d5f44fca6234f1ca67b2e63cf06ff1580188
Goldra <ultragavin229@gmail.com>
2019-09-18 21:28:30 -0700

    Update timer.js

--- timer.js
+++ timer.js
@@ -1,9 +1,9 @@
 var x = 0
-function coolsauce(){
+function unitTimer(){
 x += 1
 console.log(x);
 }
-setInterval(coolsauce, 10);
+setInterval(unitTimer, 10);
 if(x > Number.MAX_SAFE_INTEGER){
   x = infinity
 }
