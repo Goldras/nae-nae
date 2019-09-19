8c8e879356d8f50d6565788476bc47c95e7f15e6
ZachRoman <Equinoxxe@users.noreply.github.com>
2019-09-19 16:53:44 -0500

    Grid

--- grid.js
+++ grid.js
@@ -1,18 +1,38 @@
 var canvas = document.getElementById("canvas")
 var ctx = canvas.getContext("2d")
 
+function Cell(object) {
+    this.object = object
+}
+
 function generateGrid(size) {
     var grid = []
     for(var i=0;i<size;i++) {
         grid[i] = []
         for(var j=0;j<size;j++) {
-            
+            grid[i][j] = new Cell(null)
         }
     }
 }
 
 class Room {
-    constructor() {
-        this.grid = generateGrid()
+    constructor(size) {
+        this.grid = generateGrid(size)
+        this.cellWidth = canvas.width / size
     }
+}
+
+function DrawGrid() {
+    var grid = Game.currentRoom
+
+    ctx.fillStyle = "#1E1E1E"
+    ctx.fillRect(0,0,canvas.width,canvas.height)
+
+    for(var i=0;i<grid.length;i++) {
+        
+    }
+}
+
+function drawCell(cell) {
+
 }
