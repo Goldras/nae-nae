3012f1524334705c134ff174a3fcd6c6fccf9940
Goldra <ultragavin229@gmail.com>
2019-09-18 21:23:59 -0700

    Update main.js

--- main.js
+++ main.js
@@ -4,18 +4,18 @@ var hours = 0
 var days = 0
 var years = 0
 function globalTimer(){
- if(second = 60){
+ if(second == 60){
     second = 0
-    minutes = 1
-    if(minutes = 60){
+    minutes ++
+    if(minutes == 60){
         minutes = 0
-        hours = 1
-        if(hours = 24){
+        hours ++
+        if(hours == 24){
             hours = 0
-            days = 1
-            if(days = 365){
+            days ++
+            if(days == 365){
                days = 0
-               years = 1
+               years ++
         }
     }
 }
