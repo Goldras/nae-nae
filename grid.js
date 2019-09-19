4e91847eda6947b4a4520cec23d3720f26a7c23f
Zach Rimal <zachrimal@gmail.com>
2019-09-19 16:13:37 -0500

    Beige

--- grid.js
+++ grid.js
@@ -1,4 +1,18 @@
 var canvas = document.getElementById("canvas")
 var ctx = canvas.getContext("2d")
 
-var grid
+function generateGrid(size) {
+    var grid = []
+    for(var i=0;i<size;i++) {
+        grid[i] = []
+        for(var j=0;j<size;j++) {
+            
+        }
+    }
+}
+
+class Room {
+    constructor() {
+        this.grid = generateGrid()
+    }
+}
