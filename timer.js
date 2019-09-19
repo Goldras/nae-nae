e125c340f8cc201790e64d8117b92ba04714e672
Goldra <ultragavin229@gmail.com>
2019-09-18 18:27:39 -0700

    Update timer.js

--- timer.js
+++ timer.js
@@ -1,10 +1,9 @@
-var maxSafeInteger = Number.MAX_SAFE_INTEGER
 var x = 0
 function coolsauce(){
 x += 0.001
 console.log(x);
 }
 setInterval(coolsauce, 10);
-if(x > maxSafeInteger){
+if(x > Number.MAX_SAFE_INTEGER){
   x = infinity
 }
