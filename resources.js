71fee41c77540355443a609a7ea4f80bea224020
Goldras <ultragavin229@gmail.com>
2019-09-19 19:04:13 -0700

    Added resources.

--- resources.js
+++ resources.js
@@ -3,14 +3,16 @@ var resource1 = 0
 var maxWorkers = 3
 var minWorkers = 1
 var resourceOutput = 1
+
 function loadWorkers(){
- if(workers = 0){
+ if(workers == 0){
      //put quirky output message here
  }else if(loadedWorkers >= maxWorkers){
      //put quirky output here
  }else{
      workers --
      loadedWorkers ++
+     incrementWorkers(-1)
  }
 }
 function slaveLabor(){
@@ -19,5 +21,9 @@ function slaveLabor(){
     }else{
         loadedWorkers = loadedWorkers - minWorkers
         resource1 = resource1 + resourceOutput
+        incrementResources(resourceOutput)
     }
+}
+function incrementResources(n){
+
 }
